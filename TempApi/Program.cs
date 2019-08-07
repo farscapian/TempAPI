ce34af0dcd22f8b5dfa705c860fdc19c0c0c32b3
farscapian <derek@farscapian.com>
2019-08-07 15:22:54 -0500

    update

--- TempApi/Program.cs
+++ TempApi/Program.cs
@@ -19,6 +19,7 @@ namespace TempApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseSetting("https_port", "443")
             .UseStartup<Startup>();
     }
 }
--- TempApi/Startup.cs
+++ TempApi/Startup.cs
@@ -45,7 +45,7 @@ namespace TempApi
                 app.UseHsts();
             }
 
-// app.UseHttpsRedirection();
+            #app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
